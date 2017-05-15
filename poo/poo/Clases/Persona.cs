cbcf3ad37db72f5e23825da376019a4a848f85ac
felixacdc <pfmata01@gmail.com>
2017-05-15 13:53:23 -0600

    [add] inicio

---
+++ poo/poo/Clases/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo.Clases
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public string dpi { get; set; }
+
+        public Persona() { }
+
+        public Persona(string nombre, string dpi)
+        {
+            this.nombre = nombre;
+            this.dpi = dpi;
+        }
+    }
+}
